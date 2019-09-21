e17dd5c2c1eafd8d10d5e80c57923a08a99eac47
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-09-21 14:38:23 +0700

    Add store data

--- src/components/AgendaInput.vue
+++ src/components/AgendaInput.vue
@@ -1,14 +1,24 @@
 <template lang="html">
     <div class="col-md-6">
         <div class="card">
-            <div class="card-header bg-success text-white">
-                <p class="card-title">List Agenda</p>
+            <div class="card-header">
+                <h3 class="card-title">List Agenda</h3>
             </div>
-            <div class="card-body">
-                <ul>
-                    <li v-for="(row, index) in agenda" :key="index"><b>{{ row.hari }}</b> : {{ row.kegiatan }}</li>
-                </ul>
-            </div>
+            <form @submit.prevent="simpan()" class="mb-4">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label class="float-left">Hari</label>
+                        <input type="text" v-model="agenda.hari" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label class="float-left">Kegiatan</label>
+                        <input type="text" v-model="agenda.kegiatan" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <button class="float-left btn btn-danger btn-sm" :disabled="isLoading">{{ isLoading ? 'Loading...' : 'Submit' }}</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -16,10 +26,30 @@
 <script>
 export default {
     name: 'AgendaShow',
-    computed: {
-        agenda() {
-            return this.$store.state.listAgenda
+    data() {
+        return {
+            agenda: {
+                hari: '',
+                kegiatan: ''
+            }
         }
-    }
+    },
+
+    computed: {
+        isLoading() {
+            return this.$store.state.isLoading
+        }
+    },
+    
+    methods: {
+        simpan() {
+            this.$store.dispatch('simpanAgenda', this.agenda)
+            this.agenda = {
+                hari: '',
+                kegiatan: ''
+            }
+        }
+    },
+
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        isLoading: false,
         listAgenda: [
             { hari: 'senin', kegiatan: 'Belajar Vuejs', },
             { hari: 'selasa', kegiatan: 'Belajar Laravel', },
@@ -13,10 +14,18 @@ export default new Vuex.Store({
     },
 
     mutations: {
-
+        KONFIRMASI_AGENDA: (state, agenda) => {
+            state.listAgenda.push(agenda)
+        }
     },
 
     actions: {
-        
+        simpanAgenda({commit, state}, agenda) {
+            state.isLoading = true
+            setTimeout(() => {
+                commit('KONFIRMASI_AGENDA', agenda)
+                state.isLoading = false
+            }, 1500)
+        }
     }
 })
